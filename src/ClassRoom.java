cbd851e6ad0ae9fb06a14a81f3e93833aa9c00b8
devayouub <ayoubmedjahed1@gmail.com>
2025-03-23 16:55:20 +0100

    worked on the management part
    added some model classes

---
+++ src/ClassRoom.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class ClassRoom {
+         private String classnumber;
+         private int MinimumPr;
+         private int MaximumPr;
+         private ArrayList <Professor> professors;
+		public ClassRoom(String classnumber, int minimumPr, int maximumPr) {
+			if(!classnumber.matches("d{4}$")) {
+				  throw new IllegalArgumentException("classroom's number must be constitued of 4 digits");	
+				}
+			this.classnumber = classnumber;
+			this.MinimumPr = minimumPr;
+			this.MaximumPr = maximumPr;
+			this.professors = new  ArrayList <Professor> ();
+		}
+		public String getClassnumber() {
+			return classnumber;
+		}
+		public void setClassnumber(String classnumber) {
+			if(!classnumber.matches("d{4}$")) {
+				  throw new IllegalArgumentException("classroom's number must be constitued of 4 digits");	
+				}
+			this.classnumber = classnumber;
+		}
+		public int getMinimumPr() {
+			return MinimumPr;
+		}
+		public void setMinimumPr(int minimumPr) {
+			MinimumPr = minimumPr;
+		}
+		public int getMaximumPr() {
+			return MaximumPr;
+		}
+		public void setMaximumPr(int maximumPr) {
+			MaximumPr = maximumPr;
+		}
+	
+		
+}
