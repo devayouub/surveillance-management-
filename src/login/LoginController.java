b0f5ca162be38fe026dad52d3f8532377a934514
zakariajr14 <kzakijr11@gmail.com>
2025-03-28 05:49:52 +0100

    loginfinall
    das ist ein login final

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -1,5 +1,25 @@
 package login;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
 public class LoginController {
 
-}
+
+
+
+        @FXML
+        private PasswordField PasswordField;
+
+        @FXML
+        private CheckBox RememberChek;
+
+        @FXML
+        private Button buttonLogin;
+
+        @FXML
+        private TextField usernameField;
+
+
+    }
