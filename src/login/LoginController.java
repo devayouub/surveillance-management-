f15cdf585708f5f3941af284dec1fccab111b2c3
devayouub <ayoubmedjahed1@gmail.com>
2025-03-28 10:37:34 +0100

    Update LoginController.java

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -2,20 +2,23 @@ package login;
 
 import java.io.IOException;
 
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.*;
 public class LoginController {
 	        Stage stage;
 	        Scene scene;
 	        Parent root;
-	        
-	        @FXML   usernameField;
 	public void loginButtonActionListener(ActionEvent e) {
-//		-----------------switching scene ----------------------------
+//		------------------------authentication--------------------
+		
+		
+		//		-----------------switching scene ----------------------------
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("Dashboard.fxml"));
 		try {
@@ -28,18 +31,21 @@ public class LoginController {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+
 		
 		
-//		------------------------authentication--------------------
+
 		
-		String username = 
+	
 		
 		
 		
 		
 	}
-        
-        
 }
+        
+
 
     
+
+
